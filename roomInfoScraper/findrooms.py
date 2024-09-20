0bcec27e18be7b9e0f04b22439618f1d80aaf8bd
Farhan Khan <farhan.k0429@gmail.com>
2024-09-20 09:55:57 -0400

    Improved readability and moved scraper to a seperate folder

--- roomInfoScraper/findrooms.py
+++ roomInfoScraper/findrooms.py
@@ -1,66 +1,63 @@
+#Selenium Imports
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import time
-import collections
+#Beautiful Soup import 
 from bs4 import BeautifulSoup 
-import re #regular expression library to parse our data
+#Time for timer at the end before closing
+import time
  
 
 driver = webdriver.Chrome() 
 driver.get('https://classes.rutgers.edu/soc/#home');
 
 
-WebDriverWait(driver, 10).until(
+WebDriverWait(driver, 20).until(
     EC.element_to_be_clickable((By.XPATH, '//*[@id="FALL_SPRING_1_TEXT"]'))
 )
 
+formBoxes = ['//*[@id="FALL_SPRING_1_TEXT"]', '//*[@id="div-location"]/ul/li[1]/label/span', '//*[@id="level_U"]', '//*[@id="level_G"]', '//*[@id="continueButton"]']
+
 #Complete form to access all classes in New Brunswick
-selector = driver.find_element("xpath", '//*[@id="FALL_SPRING_1_TEXT"]')
-selector.click()
-selector = driver.find_element('xpath', '//*[@id="div-location"]/ul/li[1]/label/span')
-selector.click()
-selector = driver.find_element('xpath', '//*[@id="level_U"]')
-selector.click()
-selector = driver.find_element('xpath','//*[@id="level_G"]' )
-selector.click()
-selector = driver.find_element('xpath', '//*[@id="continueButton"]')
-selector.click()
+for box in formBoxes:
+    selector = driver.find_element('xpath', box)
+    selector.click()
 #Form Submitted
 
 
-# time.sleep(1.5)
+
 selector = WebDriverWait(driver, 10).until(
     EC.element_to_be_clickable((By.XPATH, '//*[@id="school_search_id"]'))
 )
 selector.click() #Click on School/Unit
 
 
-dayTime = []
 
 allClasses = []
 
 
+#Created dropdown selector for repetitive use
+dropdown = driver.find_element('xpath', '//*[@id="widget_dijit_form_FilteringSelect_1"]/div[1]/input')
 
 
 #Iterate through each school/unit
-for i in range(26):
+for school in range(26):
+
     #Click on Dropdown
-    dropdown = driver.find_element('xpath', '//*[@id="widget_dijit_form_FilteringSelect_1"]/div[1]/input')
     dropdown.click()#Click on searchbar Dropdown
     
     
-    option = WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.ID, f"dijit_form_FilteringSelect_1_popup{i}"))
-    )
+
     try:
+        option = WebDriverWait(driver, 20).until(
+        EC.element_to_be_clickable((By.ID, f"dijit_form_FilteringSelect_1_popup{school}"))
+    )
         #Click on each School
         option.click()
 
         #Wait until page is loaded before accessing data
-
-        WebDriverWait(driver, 10).until(
+        WebDriverWait(driver, 20).until(
             EC.visibility_of_element_located((By.ID, "courseDataParent"))
         )
 
@@ -80,7 +77,6 @@ for i in range(26):
                 roomInfo = [rooms[iterator].get_text(strip=True), hours[iterator].get_text(strip=True), days[iterator].get_text(strip=True)]
                 if roomInfo not in allClasses:
                     allClasses.append(roomInfo)
-                # print(rooms[iterator].get_text(strip=True), hours[iterator].get_text(strip=True), days[iterator].get_text(strip=True))
 
 
       
@@ -95,27 +91,18 @@ for i in range(26):
 
 
 
-        #className = sectionMeetingTimesDiv
-        #Room: meetingTimeBuildingAndRoom
-        #Time: meetingTimeHours
-        #Day: meetingTimeDay
+
 
 
     except Exception as e:
         print(f"Error interacting with div: {e}")
 
- 
 
 
-
-
-
-
-
-time.sleep(2)
+time.sleep(1)#Just for visibility, seeing that everything is done before closing
 
 driver.quit()
 
-with open('output.txt', 'w') as file:
+with open('roomInfoScraper/room_schedule.txt', 'w') as file:
     for item in allClasses:
         file.write(str(item) + '\n')  # Add a newline after each item
