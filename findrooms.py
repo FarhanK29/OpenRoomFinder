fccce0b25908bed7d83692abae82e5365588a97f
Farhan Khan <farhan.k0429@gmail.com>
2024-09-20 00:26:44 -0400

    Retrieved all classroom data and store in output.txt file

--- findrooms.py
+++ findrooms.py
@@ -3,9 +3,10 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
+import collections
 from bs4 import BeautifulSoup 
 import re #regular expression library to parse our data
-
+ 
 
 driver = webdriver.Chrome() 
 driver.get('https://classes.rutgers.edu/soc/#home');
@@ -29,18 +30,20 @@ selector.click()
 #Form Submitted
 
 
-# WebDriverWait(driver, 7, poll_frequency=5).until(EC.presence_of_element_located((By.XPATH, '//*[@id="footer"]/p[1]"]')), 'Timed out waiting for simple alert to appear')
-time.sleep(1.5)
+# time.sleep(1.5)
 selector = WebDriverWait(driver, 10).until(
     EC.element_to_be_clickable((By.XPATH, '//*[@id="school_search_id"]'))
 )
 selector.click() #Click on School/Unit
 
 
+dayTime = []
+
+allClasses = []
 
 
 
-i = 0
+
 #Iterate through each school/unit
 for i in range(26):
     #Click on Dropdown
@@ -67,8 +70,26 @@ for i in range(26):
         #Initialize BeautifulSoup scraper
         soup = BeautifulSoup(html_content, 'html.parser')
 
-        meeting_times_divs = soup.find_all('div', class_='sectionMeetingTimesDiv')
-        print(meeting_times_divs)
+        #Find spans of all rooms, hours, and days
+        rooms = soup.find_all('span', class_='meetingTimeBuildingAndRoom')
+        hours = soup.find_all('span', class_='meetingTimeHours')
+        days= soup.find_all('span', class_= 'meetingTimeDay') 
+
+        for iterator in range(len(rooms)):
+            if(rooms[iterator].get_text(strip=True) != ""):
+                roomInfo = [rooms[iterator].get_text(strip=True), hours[iterator].get_text(strip=True), days[iterator].get_text(strip=True)]
+                if roomInfo not in allClasses:
+                    allClasses.append(roomInfo)
+                # print(rooms[iterator].get_text(strip=True), hours[iterator].get_text(strip=True), days[iterator].get_text(strip=True))
+
+
+      
+    
+
+            
+
+
+
 
 
 
@@ -94,3 +115,7 @@ for i in range(26):
 time.sleep(2)
 
 driver.quit()
+
+with open('output.txt', 'w') as file:
+    for item in allClasses:
+        file.write(str(item) + '\n')  # Add a newline after each item
