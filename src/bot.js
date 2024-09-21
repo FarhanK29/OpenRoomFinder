7c88a067e6d14343942bfe5d7258d4f372666b18
Farhan Khan <farhan.k0429@gmail.com>
2024-09-20 21:34:44 -0400

    Created command handling for discord bot

--- src/bot.js
+++ src/bot.js
@@ -1,20 +1,14 @@
 //to read from .txt file
-const fs = require('fs');
-
-//to read info from .env
+const fs = require('node:fs');
+const path = require('node:path')
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 require('dotenv').config()
-
-//discord imports
-const {Client, IntentsBitField} = require('discord.js')
 
 // convert textFile into array 
 const data = fs.readFileSync('./roomInfoScraper/room_schedule.txt', 'utf-8').split('\n');
 
 //Initialize roomData dictionary to hold all roomData
 const roomData = {};
-
-
-
 
 for (i = 0; i < data.length; i++){
 
@@ -56,23 +50,62 @@ console.log("Monday, ARC_103", getScheduleForRoomAndDay('ARC-103', 'Monday'))
 
 const client = new Client({
   intents: [
-    IntentsBitField.Flags.Guilds,
-    IntentsBitField.Flags.GuildMembers,
-    IntentsBitField.Flags.GuildMessages,
-    IntentsBitField.Flags.MessageContent,
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
   ]
 })
 
-client.on('ready', (c) =>{
-  console.log(`${c.user.tag} is online`)
+client.commands = new Collection()
+
+//Dynamically retrieve command files
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		// Set a new item in the Collection with the key as the command name and the value as the exported module
+		if ('data' in command && 'execute' in command) {
+			client.commands.set(command.data.name, command);
+		} else {
+			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
+	}
+}
+
+
+client.on(Events.InteractionCreate, async interaction => {
+  if (!interaction.isChatInputCommand()) return;
+
+	const command = interaction.client.commands.get(interaction.commandName);
+
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+		} else {
+			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		}
+	}
+});
+
+client.once(Events.ClientReady, readyClient =>{
+  console.log(`Ready! ${readyClient.user.tag} is online`)
 })
 
-client.on('messageCreate', (message) =>{
-  if (message.content === 'fetch'){
-    console.log(getScheduleForRoomAndDay('ARC-103', 'Monday'))
-    message.reply((getScheduleForRoomAndDay('ARC-103', 'Monday').toString()))
-  }
-})
+
 
 client.login(
   process.env.DISCORD_TOKEN
